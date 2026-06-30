6ce61c55fca03bdc9764cdb939e8f32d73cb7e06
fahiiim <fahimsarker0805@gmail.com>
2026-06-30 20:12:48 +0600

    Neutrition

--- app/api/router.py
+++ app/api/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.routers.auth import router as auth_router
 from app.routers.coach_client import router as coach_client_router
 from app.routers.health import router as health_router
+from app.routers.nutrition_plan import router as nutrition_plan_router
 from app.routers.profile import router as profile_router
 from app.routers.routine import router as routine_router
 from app.routers.upload import router as upload_router
@@ -12,6 +13,7 @@ api_router = APIRouter()
 api_router.include_router(health_router)
 api_router.include_router(auth_router)
 api_router.include_router(coach_client_router)
+api_router.include_router(nutrition_plan_router)
 api_router.include_router(profile_router)
 api_router.include_router(routine_router)
 api_router.include_router(upload_router)
