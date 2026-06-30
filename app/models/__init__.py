08e81911a48f5d0054819c09ee30359bb59a8be7
fahiiim <fahimsarker0805@gmail.com>
2026-06-30 20:12:05 +0600

    Neutrition

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,12 +1,14 @@
 """Database models package."""
 
 from app.models.coach_client import CoachClient
+from app.models.nutrition_plan import NutritionPlan
 from app.models.routine import Routine
 from app.models.user import User, UserRole
 from app.models.workout_plan import WorkoutPlan, WorkoutPlanAssignment
 
 __all__ = [
 	"CoachClient",
+	"NutritionPlan",
 	"Routine",
 	"User",
 	"UserRole",
