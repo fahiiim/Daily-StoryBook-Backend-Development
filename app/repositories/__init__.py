76741d7877005fd2f517c49fb72202add805484a
fahiiim <fahimsarker0805@gmail.com>
2026-06-30 20:11:45 +0600

    Neutrition

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,12 +1,14 @@
 """Repository layer package."""
 
 from app.repositories.coach_client_repository import CoachClientRepository
+from app.repositories.nutrition_plan_repository import NutritionPlanRepository
 from app.repositories.routine_repository import RoutineRepository
 from app.repositories.user_repository import UserRepository
 from app.repositories.workout_plan_repository import WorkoutPlanRepository
 
 __all__ = [
 	"CoachClientRepository",
+	"NutritionPlanRepository",
 	"RoutineRepository",
 	"UserRepository",
 	"WorkoutPlanRepository",
