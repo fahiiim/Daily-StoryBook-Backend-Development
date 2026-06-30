845b03de53b60f5407848a6f5932f13d2bd33f63
fahiiim <fahimsarker0805@gmail.com>
2026-06-30 17:52:21 +0600

    Database Setup

--- app/api/router.py
+++ app/api/router.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 
 from app.routers.health import router as health_router
-from app.routers.test import router as test_router
 
 api_router = APIRouter()
 api_router.include_router(health_router)
-api_router.include_router(test_router)
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,1 @@
 """Database models package."""
-
-from app.models.test_table import TestTable
-
-__all__ = ["TestTable"]
