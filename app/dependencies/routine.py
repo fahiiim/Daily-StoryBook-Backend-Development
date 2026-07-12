617cab1a885284321cb1cc6d066a748dcb29eb7e
fahiiim <fahimsarker0805@gmail.com>
2026-07-12 14:17:03 +0600

    Macro setup in Routine

--- app/dependencies/routine.py
+++ app/dependencies/routine.py
@@ -2,9 +2,13 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.dependencies.db import get_db_session
+from app.repositories.routine_macro_log_repository import RoutineMacroLogRepository
 from app.repositories.routine_repository import RoutineRepository
 from app.services.routine_service import RoutineService
 
 
 def get_routine_service(db: Session = Depends(get_db_session)) -> RoutineService:
-    return RoutineService(RoutineRepository(db))
+    return RoutineService(
+        routine_repository=RoutineRepository(db),
+        routine_macro_log_repository=RoutineMacroLogRepository(db),
+    )
