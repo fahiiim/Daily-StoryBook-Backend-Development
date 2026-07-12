61a19946c680a15ae32951bdd255b4e9728ca55b
fahiiim <fahimsarker0805@gmail.com>
2026-07-12 14:17:29 +0600

    Macro setup in Routine

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,7 @@ from app.models.coach_client import CoachClient
 from app.models.nutrition_plan import NutritionPlan
 from app.models.notification import Notification, NotificationType
 from app.models.routine import Routine
+from app.models.routine_macro_log import MacroType, RoutineMacroLog
 from app.models.storybook import Storybook, StorybookStatus, StoryPage
 from app.models.subscription import Subscription, SubscriptionStatus
 from app.models.user import User, UserRole
@@ -17,6 +18,8 @@ __all__ = [
 	"Notification",
 	"NotificationType",
 	"Routine",
+	"MacroType",
+	"RoutineMacroLog",
 	"Storybook",
 	"StorybookStatus",
 	"StoryPage",
