f8a8e745a586596c16996eaea9551ec794440c5b
fahiiim <fahimsarker0805@gmail.com>
2026-07-13 15:21:18 +0600

    Coach and SELF user Profile creation

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -4,6 +4,7 @@ from app.repositories.coach_client_repository import CoachClientRepository
 from app.repositories.nutrition_plan_repository import NutritionPlanRepository
 from app.repositories.routine_macro_log_repository import RoutineMacroLogRepository
 from app.repositories.routine_repository import RoutineRepository
+from app.repositories.subscription_repository import SubscriptionRepository
 from app.repositories.user_repository import UserRepository
 from app.repositories.verification_code_repository import VerificationCodeRepository
 from app.repositories.workout_plan_repository import WorkoutPlanRepository
@@ -13,6 +14,7 @@ __all__ = [
 	"NutritionPlanRepository",
 	"RoutineMacroLogRepository",
 	"RoutineRepository",
+	"SubscriptionRepository",
 	"UserRepository",
 	"VerificationCodeRepository",
 	"WorkoutPlanRepository",
