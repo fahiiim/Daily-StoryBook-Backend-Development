790a8ae7032fe5215e99f513dac35fc4b9fa9890
fahiiim <fahimsarker0805@gmail.com>
2026-07-13 15:20:34 +0600

    Coach and SELF user Profile creation

--- app/dependencies/profile.py
+++ app/dependencies/profile.py
@@ -2,9 +2,15 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.dependencies.db import get_db_session
+from app.repositories.coach_client_repository import CoachClientRepository
+from app.repositories.subscription_repository import SubscriptionRepository
 from app.repositories.user_repository import UserRepository
 from app.services.profile_service import ProfileService
 
 
 def get_profile_service(db: Session = Depends(get_db_session)) -> ProfileService:
-    return ProfileService(UserRepository(db))
+    return ProfileService(
+        user_repository=UserRepository(db),
+        subscription_repository=SubscriptionRepository(db),
+        coach_client_repository=CoachClientRepository(db),
+    )
