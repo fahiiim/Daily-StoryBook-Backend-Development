7fde0524275b042987c98053cde2a37b9fefb96b
fahiiim <fahimsarker0805@gmail.com>
2026-07-19 16:25:19 +0600

    Routine Functionalities Rescheduled

--- app/dependencies/routine.py
+++ app/dependencies/routine.py
@@ -2,8 +2,10 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.dependencies.db import get_db_session
+from app.repositories.coach_client_repository import CoachClientRepository
 from app.repositories.routine_macro_log_repository import RoutineMacroLogRepository
 from app.repositories.routine_repository import RoutineRepository
+from app.repositories.user_repository import UserRepository
 from app.services.routine_service import RoutineService
 
 
@@ -11,4 +13,6 @@ def get_routine_service(db: Session = Depends(get_db_session)) -> RoutineService
     return RoutineService(
         routine_repository=RoutineRepository(db),
         routine_macro_log_repository=RoutineMacroLogRepository(db),
+        user_repository=UserRepository(db),
+        coach_client_repository=CoachClientRepository(db),
     )
