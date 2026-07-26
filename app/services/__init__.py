cd1ff8af74f21eb541e0d9f32c97d0879680e377
fahiiim <fahimsarker0805@gmail.com>
2026-07-26 15:14:28 +0600

    Weekly summary Logic Revamped

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -87,6 +87,7 @@ from app.services.verification_flow_service import (
 	VerificationFlowServiceError,
 	VerificationUserNotFoundError,
 )
+from app.services.weekly_summary_service import DailyGoalNotFoundError
 
 __all__ = [
 	"AuthService",
@@ -154,4 +155,5 @@ __all__ = [
 	"VerificationFlowService",
 	"VerificationFlowServiceError",
 	"VerificationUserNotFoundError",
+	"DailyGoalNotFoundError",
 ]
